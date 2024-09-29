44392e537d1cf43170a34399318f7b6fe836a44c
Aminul-Islam7 <aminulfardin7@gmail.com>
2024-09-29 22:41:17 +0600

    refactor: Make image file selection logic more versatile

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
 'use strict';
 
-let numberOfImages = 10;
-let currentImage = 1;
+// Defines array of filenames and starting index
+let images = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', '9.jpg', '10.jpg'];
+let imageIndex = 0;
 
 let loadPreviousImage = function () {
-	currentImage = --currentImage < 1 ? numberOfImages : currentImage;
-	setImage(currentImage);
+	// selects previous index if it exists and calls setImage with the corresponding filename
+	if (imageIndex > 0) imageIndex--;
+	setImage(images[imageIndex]);
 };
 
 let loadNextImage = function () {
-	currentImage = ++currentImage > numberOfImages ? 1 : currentImage;
-	setImage(currentImage);
+	// selects next index if it exists and calls setImage with the corresponding filename
+	if (imageIndex < images.length - 1) imageIndex++;
+	setImage(images[imageIndex]);
 };
 
-let setImage = function (currentImage) {
-	document.querySelector('.image').src = `images/${currentImage}.jpg`;
-	document.querySelector('body').style.backgroundImage = `url(/images/${currentImage}.jpg)`;
+let setImage = function (fileName) {
+	document.querySelector('.image').src = `images/${fileName}`; // updates the slider image according to selected filename
+	document.querySelector('body').style.backgroundImage = `url(/images/${fileName})`; // similarly updates the backdrop image
 };
 
+// Click actions of navigation buttons
 document.querySelector('.btn-prev').addEventListener('click', loadPreviousImage);
 document.querySelector('.btn-next').addEventListener('click', loadNextImage);
