2ad57affeed9154ae09d458884af96c058b9baee
Aminul-Islam7 <aminulfardin7@gmail.com>
2024-09-29 21:21:27 +0600

    feat: Add functionality to change images

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 'use strict';
+
+let numberOfImages = 10;
+let currentImage = 1;
+
+let loadPreviousImage = function () {
+	currentImage = --currentImage < 1 ? numberOfImages : currentImage;
+	setImage(currentImage);
+};
+
+let loadNextImage = function () {
+	currentImage = ++currentImage > numberOfImages ? 1 : currentImage;
+	setImage(currentImage);
+};
+
+let setImage = function (currentImage) {
+	document.querySelector('.image').src = `images/${currentImage}.jpg`;
+	document.querySelector('body').style.backgroundImage = `url(images/${currentImage}.jpg)`;
+};
+
+document.querySelector('.btn-prev').addEventListener('click', loadPreviousImage);
+document.querySelector('.btn-next').addEventListener('click', loadNextImage);
